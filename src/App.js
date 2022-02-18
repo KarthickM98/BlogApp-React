a5b39f115138cbb0e9230d051edad7a1749b573a
Karthikeyan <karthikeyan@Karthikeyans-MacBook-Air.local>
2022-02-18 14:59:46 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-// import "./Component/blogStyles.css"
 import { BrowserRouter as Router, Route, Routes,Navigate } from "react-router-dom";
 import { Context } from "./Component/Context";
 import { Header } from "./Component/Header"; // name expo component needs{}// className="App-header"
 import { Home } from "./Component/Home"
 import { Bollywood } from "./Component/Category";
-// import { Technology } from "./Component/Technology";
-// import { Fitness } from "./Component/Fitness";
-// import { Hollywood } from "./Component/Hollywood";
-// import { Food } from "./Component/Food";
 import { PageNotFound } from "./Component/PageNotFound";
 import { ShowContent } from "./Component/ShowContent";
 
@@ -21,10 +16,6 @@ function App() {// header is not route component//
           <Route path ='/' element ={<Navigate to ='/home'/>}/>
           <Route path ='/home' element={<Home/>}/>
           <Route path ='title/:category' element={<Bollywood/>}/>
-          {/* <Route path ='/technology' element={<Technology/>}/>
-          <Route path ='/hollywood' element={<Hollywood/>}/>
-          <Route path ='/fitness' element={<Fitness/>}/>
-          <Route path ='/food' element={<Food/>}/> */}
           <Route path ='/:show/:id' element={<ShowContent/>}/>
           <Route path ='/*' element={<PageNotFound/>}/>
         </Routes>
